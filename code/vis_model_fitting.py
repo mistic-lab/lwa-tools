56286d90ba5e3720f54be7e2f09414bb5c57d37f
Jeff <jtst@uvic.ca>
2020-08-15 21:06:18 -0700

    Add command line interface for model fitting

--- code/vis_model_fitting.py
+++ code/vis_model_fitting.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python2
 
+import argparse
 import numpy as np
 from scipy.optimize import least_squares
 import plotly.graph_objects as go
 from lsl.common import stations
 
 from generate_visibilities import compute_visibilities_gen
-from known_transmitters import get_transmitter_coords
+import known_transmitters
 
 #tbn_filename = "../../data/058846_00123426_s0020.tbn"
 #target_freq = 5351500
@@ -14,15 +15,13 @@ from known_transmitters import get_transmitter_coords
 #l_guess = 0.0035 # from a manual fit to the first integration
 #m_guess = 0.007
 
-tbn_filename = "../../data/058628_001748318.tbn"
-target_freq = 10e6
-transmitter_coords = get_transmitter_coords('WWV')
-l_guess = 0.008 # from a manual fit to the first integration
-m_guess = 0.031
+#tbn_filename = "../../data/058628_001748318.tbn"
+#target_freq = 10e6
+#transmitter_coords = get_transmitter_coords('WWV')
+#l_guess = 0.008 # from a manual fit to the first integration
+#m_guess = 0.031
 
 station = stations.lwasv
-
-export_scatters = [12, 15, 19]
 
 def point_source_visibility_model(u, v, l, m):
     '''
@@ -70,18 +69,34 @@ def lm_to_ea(l, m):
 
     return elev, azimuth
 
-if __name__ == "__main__":
-    bearing, _, distance = station.getPointingAndDistance(transmitter_coords + [0])
+def main(args):
+    transmitter_coords = known_transmitters.parse_args(args)
+    if transmitter_coords:
+        bearing, _, distance = station.getPointingAndDistance(transmitter_coords + [0])
+    else:
+        print("Please specify a transmitter location")
+        return
+
+    if args.csv:
+        print("Writing CSV output to {}".format(args.csv))
+        csvf = open(args.csv, 'w')
+        csvf.write("integration,l_i,m_i,l_o,m_o,elev_o,az_o,az_straight,distance,height,cost\n")
+    else:
+        print("No output file specified.")
+        
 
     # arrays for estimated parameters from each integration
-    l_est = np.array([l_guess])
-    m_est = np.array([m_guess])
+    l_est = np.array([args.l_guess])
+    m_est = np.array([args.m_guess])
+    elev_est = np.array([])
+    az_est = np.array([])
+    height_est = np.array([])
 
     k = 0
 
     divs = []
 
-    for bl, vis in compute_visibilities_gen(tbn_filename, target_freq):
+    for bl, vis in compute_visibilities_gen(args.tbn_filename, args.target_freq):
         
         # extract the baseline measurements from the baseline object pairs
         bl2d = np.array([np.array([b[0].stand.x - b[1].stand.x, b[0].stand.y - b[1].stand.y]) for b in bl])
@@ -106,11 +121,31 @@ if __name__ == "__main__":
         print("Optimization result: {}".format(opt_result))
         print("Start point: {}".format((l_init, m_init)))
         l_out, m_out = opt_result['x']
+        cost = opt_result['cost']
 
         l_est = np.append(l_est, l_out)
         m_est = np.append(m_est, m_out)
 
-        if k in export_scatters:
+        elev, az = lm_to_ea(l_out, m_out)
+
+        # use the flat mirror ionosphere model for now
+        # TODO: tilted mirror model?
+        height = distance * np.tan(elev)/2.0
+
+        csvf.write("{},{},{},{},{},{},{},{},{},{},{}\n".format(
+            k,
+            l_init,
+            m_init,
+            l_out,
+            m_out,
+            elev,
+            az,
+            bearing,
+            distance,
+            height,
+            cost))
+
+        if k in args.scatter:
             print("Plotting model and data scatter")
             data = [
                 go.Scatter3d(x=u, y=v, z=np.angle(vis), mode='markers', marker=dict(size=1, color='red')),
@@ -130,15 +165,37 @@ if __name__ == "__main__":
         k += 1
         print("\n\n")
 
-    # remove our initial guess
-    l_est = l_est[1:]
-    m_est = m_est[1:]
+    csvf.close()
 
-    elev, az = lm_to_ea(l_est, m_est)
+    ## remove our initial guess
+    #l_est = l_est[1:]
+    #m_est = m_est[1:]
 
-    # use the flat mirror ionosphere model for now
-    height = distance * np.tan(elev) / 2.0
+    #elev, az = lm_to_ea(l_est, m_est)
+
+    #height = distance * np.tan(elev) / 2.0
 
 
-    # TODO: tilted mirror model?
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="compute visibilities and fit a model to them",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            fromfile_prefix_chars='@'
+            )
+    parser.add_argument('tbn_filename', type=str,
+            help='name of TBN data file')
+    parser.add_argument('--csv', '-c', type=str,
+            help='name of output CSV file')
+    parser.add_argument('target_freq', type=float,
+            help='transmitter frequency')
+    parser.add_argument('l_guess', type=float,
+            help='initial guess for l parameter')
+    parser.add_argument('m_guess', type=float,
+            help='initial guess for m parameter')
+    parser.add_argument('scatter', type=int, nargs='*',
+            help='export scatter plots for these integrations')
+            
+    known_transmitters.add_args(parser)
+    args = parser.parse_args()
+    main(args)
