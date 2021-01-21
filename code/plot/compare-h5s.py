b8814da8ee3ef5e3791298243e5248aeda7b0586
Jeff Stacey <jeffstacey111@gmail.com>
2021-01-21 15:44:07 -0800

    add difference plotting to compare-h5s.py

--- code/plot/compare-h5s.py
+++ code/plot/compare-h5s.py
@@ -19,24 +19,7 @@ def main(args):
     h1  = f1['height'][:]
     fig, ax = plt.subplots(5,1)
 
-    ax[0].set_title(r'$l$')
-    ax[0].plot(l1, 'r', label=args.label1)
-
-    ax[1].set_title(r'$m$')
-    ax[1].plot(m1, 'r')
-
-    ax[2].set_title('Elevation')
-    ax[2].plot(el1, 'r')
-
-    ax[3].set_title('Azimuth')
-    ax[3].plot(az1, 'r')
-
-    ax[4].set_title('Height')
-    ax[4].plot(h1, 'r')
-
     if args.file2 is not None:
-        if args.label2 is None:
-             raise RuntimeError("Need a label '-l2' for file2")
         f2=h5py.File(args.file2, 'r')
         l2  = f2['l_est'][:]
         m2  = f2['m_est'][:]
@@ -44,27 +27,51 @@ def main(args):
         az2 = f2['azimuth'][:]
         h2  = f2['height'][:]
 
-        ax[0].plot(l2, 'k', label=args.label2)
-        ax[1].plot(m2, 'k')
-        ax[2].plot(el2, 'k')
-        ax[3].plot(az2, 'k')
-        ax[4].plot(h2, 'k')
+    ax[0].set_title(r'$l$')
+    ax[1].set_title(r'$m$')
+    ax[2].set_title('Elevation')
+    ax[3].set_title('Azimuth')
+    ax[4].set_title('Height')
 
-    if args.file3 is not None:
-        if args.label3 is None:
-             raise RuntimeError("Need a label '-l3' for file3")
-        f3=h5py.File(args.file3, 'r')
-        l3  = f3['l_est'][:]
-        m3  = f3['m_est'][:]
-        el3 = f3['elevation'][:]
-        az3 = f3['azimuth'][:]
-        h3  = f3['height'][:]
+    if args.diff:
+        if not args.file2 or args.file3:
+            raise RuntimeError("Need to supply file1 and file2 but not file3 for --diff")
+        ax[0].plot(l1 - l2, 'k', label=f"{args.label1} - {args.label2}")
+        ax[1].plot(m1 - m2, 'k')
+        ax[2].plot(el1 - el2, 'k')
+        ax[3].plot(az1 - az2, 'k')
+        ax[4].plot(h1 - h2, 'k')
+    else: 
+        ax[0].plot(l1, 'r', label=args.label1)
+        ax[1].plot(m1, 'r')
+        ax[2].plot(el1, 'r')
+        ax[3].plot(az1, 'r')
+        ax[4].plot(h1, 'r')
 
-        ax[0].plot(l3, 'b', label=args.label2)
-        ax[1].plot(m3, 'b')
-        ax[2].plot(el3, 'b')
-        ax[3].plot(az3, 'b')
-        ax[4].plot(h3, 'b')
+        if args.file2 is not None:
+            if args.label2 is None:
+                 raise RuntimeError("Need a label '-l2' for file2")
+            ax[0].plot(l2, 'k', label=args.label2)
+            ax[1].plot(m2, 'k')
+            ax[2].plot(el2, 'k')
+            ax[3].plot(az2, 'k')
+            ax[4].plot(h2, 'k')
+
+        if args.file3 is not None:
+            if args.label3 is None:
+                 raise RuntimeError("Need a label '-l3' for file3")
+            f3=h5py.File(args.file3, 'r')
+            l3  = f3['l_est'][:]
+            m3  = f3['m_est'][:]
+            el3 = f3['elevation'][:]
+            az3 = f3['azimuth'][:]
+            h3  = f3['height'][:]
+
+            ax[0].plot(l3, 'b', label=args.label2)
+            ax[1].plot(m3, 'b')
+            ax[2].plot(el3, 'b')
+            ax[3].plot(az3, 'b')
+            ax[4].plot(h3, 'b')
 
     handles, labels = ax[0].get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper right')
@@ -90,6 +97,8 @@ if __name__ == "__main__":
             help='label for second HDF5 file')
     parser.add_argument('--label3', '-l3', type=str, default=None,
             help='label for possible third HDF5 file')
+    parser.add_argument('--diff', '-d', action='store_true',
+            help='plot difference between file1 and file2')
 
 
     args = parser.parse_args()
