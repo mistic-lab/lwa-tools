b3ff091100aa287d2655c08fad90a501c3e854ee
Jeff <jtst@uvic.ca>
2021-02-12 14:56:00 -0800

    change kernel parametrization in the gridder

--- lwatools/imaging/grid.py
+++ lwatools/imaging/grid.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def gauss_kernel_1d(offset, extent, sigma=1):
+def gauss_kernel_1d(extent, res=0.5, alpha=2):
     '''
-    computes a sampled gaussian kernel with an offset
+    computes a sampled gaussian kernel
 
     offset should be between -1 and 1 and indicates how far the peak of the
     kernel should be offset from the middle of the array
@@ -11,7 +11,14 @@ def gauss_kernel_1d(offset, extent, sigma=1):
     index extent//2
     '''
     x = np.arange(extent) - extent//2
-    g = 1/(sigma * np.sqrt(np.pi)) * np.exp(-0.5 * (x - offset)**2 / sigma**2)
+    # standard gaussian kernel with sigma parameter
+    # sigma = 0.5 matches LSL kernel
+    #g = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * x**2 / sigma**2)
+
+    # gaussian kernel from Thompson/Moran pg. 498
+    # LSL kernel could be equal if it square rooted alpha in the denominator
+    g = 1/(alpha * res * np.sqrt(np.pi)) * np.exp(-(x/(alpha * res))**2)
+
     return g
 
 def grid_point_with_offset(u, v, w, vis, kernel_1d, kernel_extent, res):
@@ -41,7 +48,7 @@ def grid_point_with_offset(u, v, w, vis, kernel_1d, kernel_extent, res):
 
     return out, center_u, center_v
     
-def grid_vis(uvw, vis, size=80, res=0.5, extent=7, sigma=0.5):
+def grid_vis(uvw, vis, size=80, res=0.5, extent=7, alpha=np.sqrt(2)):
     '''
     note: neglects W coordinate completely - shouldn't be a problem, right?
     '''
@@ -55,12 +62,14 @@ def grid_vis(uvw, vis, size=80, res=0.5, extent=7, sigma=0.5):
     blv = uvw[:, 1]
     blw = uvw[:, 2]
 
-    # generate the 1d gaussian kernel which will be used to find the 2d kernel points
-    gk1d = gauss_kernel_1d(0, extent, sigma=sigma)
+    # generate the 1d kernel which will be used to find the 2d kernel points
+    kern1d = gauss_kernel_1d(extent, res=res, alpha=alpha)
+
     
-    # the 2d gaussian kernel at (i,j) is gk1d(i) * gk2d(j), we can use the outer product
-    gk1d.shape = (gk1d.shape[0], 1)
-    gk2d = np.matmul(gk1d, gk1d.T)
+    # the 2d kernel is separable, so kern2d(i,j) = kern1d(i) * kern1d(j)
+    # we can use the outer product to evaluate
+    kern1d.shape = (kern1d.shape[0], 1)
+    kern2d = np.matmul(kern1d, kern1d.T)
 
     for k in range(len(vis)):
         uk = uvw[k, 0]
@@ -69,6 +78,6 @@ def grid_vis(uvw, vis, size=80, res=0.5, extent=7, sigma=0.5):
         center_u = int(np.round(uk/res) + size/(2*res) )
         center_v = int(np.round(vk/res) + size/(2*res) )
 
-        grvis[center_u - extent//2 : center_u + extent//2 + 1, center_v - extent//2 : center_v + extent//2 + 1] += gk2d * visk
+        grvis[center_u - extent//2 : center_u + extent//2 + 1, center_v - extent//2 : center_v + extent//2 + 1] += kern2d * visk
 
     return gru, grv, grvis
