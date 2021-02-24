f04cee022ef4877edcebbb0f1fb7a8dee2306738
Jeff <jtst@uvic.ca>
2021-02-23 16:19:44 -0800

    add reflection point calculations to reflection models

--- lwatools/ionospheric_models/fixed_dist_mirrors.py
+++ lwatools/ionospheric_models/fixed_dist_mirrors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from geographiclib.geodesic import Geodesic
 
 def flatmirror_height(elev, tx_dist):
     '''
@@ -7,6 +8,28 @@ def flatmirror_height(elev, tx_dist):
     '''
     return (tx_dist/2) * np.tan(elev)
 
+def flatmirror_location(tx_coords, rx_coords):
+    '''
+    This is an easy one - it's just halfway along the great circle between TX and RX.
+
+    Arguments should be (lat, long) pairs in decimal degrees.
+    '''
+
+    tx_lat, tx_lon = tx_coords
+    rx_lat, rx_lon = rx_coords
+
+    # point 1 (lat1, lon1, etc) is the receiver
+    # point 2 (lat2, lon2, etc) is the transmitter
+    # the inverse problem gives us the length and azimuths of the line connecting two given points
+    # note that the azimuth that the line points is different at each point
+    rx_to_tx = Geodesic.WGS84.Inverse(rx_lat, rx_lon, tx_lat, tx_lon)
+
+    # the direct problem takes a starting point and a line and tells us the endpoint
+    # we'll give it the receiver location and the azimuth and distance determined above
+    rx_to_midpoint = Geodesic.WGS84.Direct(rx_lat, rx_lon, rx_to_tx['azi1'], rx_to_tx['s12']/2)
+
+    return rx_to_midpoint['lat2'], rx_to_midpoint['lon2']
+
 def tiltedmirror_height(elev, az, tx_az, tx_dist):
     '''
     Computes the reflection virtual height using a tilted mirror model.
@@ -14,3 +37,29 @@ def tiltedmirror_height(elev, az, tx_az, tx_dist):
     '''
     return (tx_dist/2) * np.tan(elev) / np.cos(az - tx_az)
 
+def tiltedmirror_location(tx_coords, rx_coords, az):
+    '''
+    Coordinate arguments should be (lat, long) pairs in decimal degrees.
+
+    az should be the azimuth of arrival in degrees
+    '''
+
+    tx_lat, tx_lon = tx_coords
+    rx_lat, rx_lon = rx_coords
+
+    # get the line between rx and xx
+    rx_to_tx = Geodesic.WGS84.Inverse(rx_lat, rx_lon, tx_lat, tx_lon)
+
+    # find the midpoint of the rx to tx line
+    rx_to_midpoint = Geodesic.WGS84.Direct(rx_lat, rx_lon, rx_to_tx['azi1'], rx_to_tx['s12']/2,
+            outmask = Geodesic.WGS84.STANDARD | Geodesic.WGS84.REDUCEDLENGTH )
+
+    # rx_to_midpoint contains the "reduced length" m12, which indicates how much a perturbation in azimuth results in a perpendicular offset as we travel along the geodesic
+    # we know the azimuth perturbation that points towards the reflection point, so we can find how far off of the rx to tx line the reflection is
+    # annoyingly m12 is in radians....
+    az_offset = az - rx_to_midpoint['azi1']
+    perp_distance = rx_to_midpoint['m12'] * np.pi/180 * az_offset
+    # now we can use the direct problem again from the midpoint along the perpendicular to the tx_to_rx line to find the reflection point
+    midpoint_to_refl = Geodesic.WGS84.Direct(rx_to_midpoint['lat2'], rx_to_midpoint['lon2'], rx_to_midpoint['azi2'] + 90.0, perp_distance)
+
+    return midpoint_to_refl['lat2'], midpoint_to_refl['lon2']
