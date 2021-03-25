49d076c5da50a18d3c360660b44c7b50281a441f
Nicholas Bruce <nsbruce@uvic.ca>
2021-03-25 16:04:58 -0700

    Simplifies grid_vis for imaging

--- lwatools/imaging/image_model_fitting.py
+++ lwatools/imaging/image_model_fitting.py
@@ -18,71 +18,12 @@ from lsl.sim import vis as simVis
 
 from lwatools.file_tools.outputs import build_output_file
 from lwatools.visibilities.models import point_source_visibility_model_uv
-from lwatools.imaging.imaging_utils import lm_to_ea, get_gimg_max, get_gimg_center_of_mass
+from lwatools.imaging.imaging_utils import lm_to_ea, get_gimg_max, get_gimg_center_of_mass, grid_visibilities
 from lwatools.ionospheric_models.fixed_dist_mirrors import flatmirror_height, tiltedmirror_height
 from lwatools.visibilities.baselines import uvw_from_antenna_pairs
 from lwatools.visibilities.generate import compute_visibilities_gen, select_antennas
 from lwatools.utils import known_transmitters
 
-def grid_visibilities(bl, freqs, vis, tx_freq, jd, valid_ants, station, size=80, res=0.5, wres=0.10, use_pol=0):
-    '''
-    Resamples the baseline-sampled visibilities on to a regular grid. 
-
-    arguments:
-    bl = pairs of antenna objects representing baselines (list)
-    freqs = frequency channels for which we have correlations (list)
-    vis = visibility samples corresponding to the baselines (numpy array)
-    target_bin = the bin we want to use (number)
-    jd = the date - shouldn't actually be important, but VisibilityDataSet needs it (number)
-    valid_ants = which antennas we actually want to use (list)
-    station = lsl station object - usually stations.lwasv
-    according to LSL docstring:
-        size = number of wavelengths which the UV matrix spans (this 
-        determines the image resolution).
-        res = resolution of the UV matrix (determines image field of view).
-        wres: the gridding resolution of sqrt(w) when projecting to w=0.
-
-    use_pol = which polarization to use (only 0 is supported right now)
-    returns:
-    gridded_image
-    '''
-    # In order to do the gridding, we need to build a VisibilityDataSet using
-    # lsl.imaging.data.VisibilityDataSet. We have to build a bunch of stuff to
-    # pass to its constructor.
-
-    # we only want the bin nearest to our frequency
-    target_bin = np.argmin([abs(tx_freq - f) for f in freqs])
-
-    # Build antenna array
-    antenna_array = simVis.build_sim_array(station, valid_ants, freqs/1e9, jd=jd, force_flat=True)
-
-    uvw = np.empty((len(bl), 3, len(freqs)))
-
-    for i, f in enumerate(freqs):
-        # wavelength = 3e8/f # TODO this should be fixed. What is currently happening is not true. Well it is, but only if you're looking for a specific transmitter frequency. Which I guess we are. I just mean it's not generalized.
-        wavelength = 3e8/tx_freq
-        uvw[:,:,i] = uvw_from_antenna_pairs(bl, wavelength=wavelength)
-
-
-    dataSet = VisibilityDataSet(jd=jd, freq=freqs, baselines=bl, uvw=uvw, antennarray=antenna_array)
-    if use_pol == 0:
-        pol_string = 'XX'
-        p=0 # this is related to the enumerate in lsl.imaging.utils.CorrelatedIDI().get_data_set() (for when there are multiple pols in a single dataset)
-    else:
-        raise RuntimeError("Only pol. XX supported right now.")
-    polDataSet = PolarizationDataSet(pol_string, data=vis)
-    dataSet.append(polDataSet)
-
-
-    # Use lsl.imaging.utils.build_gridded_image (takes a VisibilityDataSet)
-    gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=size, res=res, wres=wres)
-
-    # gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=100, res=0.3) #what I think it had ought to be if size=N and res=du
-    # gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=3, res=0.01) #what I think it had ought to be if the docstring is true
-    # gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=10, res=0.05) #what I think it had ought to be if the docstring is true
-    # gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=20, res=0.5) #from sim
-
-    return gridded_image
 
 def main(args):
     station = stations.lwasv
@@ -126,12 +67,12 @@ def main(args):
     for bl, freqs, vis in compute_visibilities_gen(tbnf, valid_ants, integration_length=args.integration_length, fft_length=args.fft_len, use_pol=args.use_pol, use_pfb=args.use_pfb):
 
         # Normalize amplitudes since we want it based on phase
-        vis/=np.abs(vis)
+        # vis/=np.abs(vis)
 
 
         jd = tbnf.get_info('start_time').jd
 
-        gridded_image = grid_visibilities(bl, freqs, vis, args.tx_freq, jd, valid_ants, station)
+        gridded_image = grid_visibilities(bl, freqs, vis, args.tx_freq, jd, station)
 
 
         save_all_sky = (args.all_sky and k in args.all_sky) or (args.all_sky_every and k % args.all_sky_every == 0)
--- lwatools/imaging/imaging_utils.py
+++ lwatools/imaging/imaging_utils.py
@@ -2,6 +2,13 @@ import numpy as np
 import warnings
 from scipy.ndimage import center_of_mass
 
+from lsl.imaging.data import VisibilityDataSet, PolarizationDataSet
+from lsl.imaging.analysis import find_point_sources
+from lsl.imaging.utils import build_gridded_image, plot_gridded_image
+from lsl.sim import vis as simVis
+
+from lwatools.visibilities.generate import compute_visibilities_gen, select_antennas
+from lwatools.visibilities.baselines import uvw_from_antenna_pairs
 
 def lm_to_ea(l, m):
     '''
@@ -73,3 +80,61 @@ def get_gimg_center_of_mass(gridded_image, return_img=False, weighting='natural'
         return l, m
     else:
         return l,m, img, extent
+
+def grid_visibilities(bl, freqs, vis, tx_freq, station, size=80, res=0.5, wres=0.10, use_pol=0, jd=None):
+    '''
+    Resamples the baseline-sampled visibilities on to a regular grid. 
+
+    arguments:
+    bl = pairs of antenna objects representing baselines (list)
+    freqs = frequency channels for which we have correlations (list)
+    vis = visibility samples corresponding to the baselines (numpy array)
+    target_bin = the bin we want to use (number)
+    jd = the date - shouldn't actually be important, but VisibilityDataSet needs it (number)
+    valid_ants = which antennas we actually want to use (list)
+    station = lsl station object - usually stations.lwasv
+    according to LSL docstring:
+        size = number of wavelengths which the UV matrix spans (this 
+        determines the image resolution).
+        res = resolution of the UV matrix (determines image field of view).
+        wres: the gridding resolution of sqrt(w) when projecting to w=0.
+
+    use_pol = which polarization to use (only 0 is supported right now)
+    returns:
+    gridded_image
+    '''
+    # In order to do the gridding, we need to build a VisibilityDataSet using
+    # lsl.imaging.data.VisibilityDataSet. We have to build a bunch of stuff to
+    # pass to its constructor.
+
+    valid_ants, n_baselines = select_antennas(station.antennas, use_pol)
+
+
+    # we only want the bin nearest to our frequency
+    target_bin = np.argmin([abs(tx_freq - f) for f in freqs])
+
+    # Build antenna array
+    freqs = np.array(freqs)
+    antenna_array = simVis.build_sim_array(station, valid_ants, freqs/1e9, jd=jd, force_flat=True)
+
+    uvw = np.empty((len(bl), 3, len(freqs)))
+
+    for i, f in enumerate(freqs):
+        # wavelength = 3e8/f # TODO this should be fixed. What is currently happening is not true. Well it is, but only if you're looking for a specific transmitter frequency. Which I guess we are. I just mean it's not generalized.
+        wavelength = 3e8/tx_freq
+        uvw[:,:,i] = uvw_from_antenna_pairs(bl, wavelength=wavelength)
+
+
+    dataSet = VisibilityDataSet(jd=jd, freq=freqs, baselines=bl, uvw=uvw, antennarray=antenna_array)
+    if use_pol == 0:
+        pol_string = 'XX'
+    else:
+        raise RuntimeError("Only pol. XX supported right now.")
+    polDataSet = PolarizationDataSet(pol_string, data=vis)
+    dataSet.append(polDataSet)
+
+
+    # Use lsl.imaging.utils.build_gridded_image (takes a VisibilityDataSet)
+    gridded_image = build_gridded_image(dataSet, pol=pol_string, chan=target_bin, size=size, res=res, wres=wres)
+
+    return gridded_image
